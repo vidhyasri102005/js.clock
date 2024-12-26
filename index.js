ff2ea2d02d58b77c1416cbe1364c6b02c924a168
vidhyasri102005 <vidhyasri102005@gmail.com>
2024-12-26 20:45:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const currentDate = new Date();
+let hours = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+let seconds = currentDate.getSeconds();
+minutes = minutes < 10 ? '0' + minutes : minutes;
+seconds = seconds < 10 ? '0' + seconds : seconds;
+let ampm = hours >= 12 ? 'PM' : 'AM';
+hours = hours % 12;
+hours = hours ? hours : 12; 
+const formattedTime = `${hours}:${minutes}:${seconds} ${ampm}`;
+console.log(formattedTime);
